dd364ccd040f5e69f75653c1edc345228070fa38
nadav152 <nadav@ametos.com>
2023-05-17 23:08:34 +0300

    Refactor naming & added notes

--- api/cache.py
+++ api/cache.py
@@ -1,11 +1,16 @@
-from api.models import Book
-from typing import Union
+from typing import Optional, Any
 import threading
 import time
 import heapdict
 
 
-class BookCache:
+class Cache:
+    """
+    This cache class supports saving DB models objects or object with ID
+    :param size: will set the cache size.
+    :param default_ttl: will set the TTL for objects in the cache.
+    """
+
     def __init__(self, size: int = 3, default_ttl: int = 60):
         self.cache = {}
         self.size = size
@@ -13,38 +18,38 @@ class BookCache:
         self.access_time = heapdict.heapdict()
         self.lock = threading.Lock()
 
-    def get_book(self, book_id: int) -> Union[Book, None]:
-        if book_id in self.cache:
+    def get_object(self, object_id: int) -> Optional[Any]:
+        if object_id in self.cache:
             with self.lock:
-                book_access_time = time.time()
-                self.access_time[book_id] = book_access_time
-            self.__create_delete_timer(book_id, book_access_time)
-            return self.cache[book_id]
+                object_access_time = time.time()
+                self.access_time[object_id] = object_access_time
+            self.__create_delete_timer(object_id, object_access_time)
+            return self.cache[object_id]
         else:
             return None
 
-    def add_book(self, book_id: int, book: Book) -> None:
+    def set_object(self, object_id: int, object_model: Any) -> None:
         with self.lock:
-            if book_id not in self.cache:
+            if object_id not in self.cache:
                 self.__ensure_cache_size()
-                self.cache[book_id] = book
+                self.cache[object_id] = object_model
 
-            book_access_time = time.time()
-            self.access_time[book_id] = book_access_time
-        self.__create_delete_timer(book_id, book_access_time)
+            object_access_time = time.time()
+            self.access_time[object_id] = object_access_time
+        self.__create_delete_timer(object_id, object_access_time)
 
-    def remove_book(self, book_id: int, periodic_delete_time: float = None) -> None:
-        is_delete_from_cache = book_id in self.cache
+    def remove_object(self, object_id: int, periodic_delete_time: float = None) -> None:
+        is_delete_from_cache = object_id in self.cache
         with self.lock:
-            # the deal with the case of reentering the book to the cache
+            # the deal with the case of reentering the same object to the cache
             if periodic_delete_time is not None:
                 is_delete_from_cache = (
-                    is_delete_from_cache and self.access_time[book_id] == periodic_delete_time
+                    is_delete_from_cache and self.access_time[object_id] == periodic_delete_time
                 )
 
             if is_delete_from_cache:
-                del self.cache[book_id]
-                del self.access_time[book_id]
+                del self.cache[object_id]
+                del self.access_time[object_id]
 
     # not part of requirement but in real life might be needed
     def invalidate_cache(self) -> None:
@@ -52,9 +57,9 @@ class BookCache:
             self.cache.clear()
             self.access_time.clear()
 
-    def __create_delete_timer(self, book_id: int, book_access_time: float) -> None:
+    def __create_delete_timer(self, object_id: int, object_access_time: float) -> None:
         curr_thread = threading.Timer(
-            self.default_ttl, self.remove_book, args=(book_id, book_access_time)
+            self.default_ttl, self.remove_object, args=(object_id, object_access_time)
         )
         curr_thread.setDaemon(True)
         curr_thread.start()
@@ -63,6 +68,6 @@ class BookCache:
         if len(self.cache) >= self.size:
             # this can also be implement with bidirectional linked list to achieve O(1) performance
             # using bidirectional linked list increases the memory usage
-            oldest_book_id, oldest_book_time = self.access_time.peekitem()
-            del self.cache[oldest_book_id]
-            del self.access_time[oldest_book_id]
+            oldest_object_id, oldest_object_time = self.access_time.peekitem()
+            del self.cache[oldest_object_id]
+            del self.access_time[oldest_object_id]
--- api/test/test_cache.py
+++ api/test/test_cache.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from api.models import Book
-from api.cache import BookCache
+from api.cache import Cache
 from typing import Union
 from threading import Thread
 import time
@@ -8,50 +8,50 @@ import time
 
 class CacheTesting(TestCase):  # django is creating new DB
     def test_add_get_cache_book(self):
-        book_cache = BookCache(size=3, default_ttl=1)
+        book_cache = Cache(size=3, default_ttl=1)
         book_1 = Book.objects.create(name='name', author='me', pages_amount=5)
-        book_cache.add_book(book_id=book_1.id, book=book_1)
+        book_cache.set_object(object_id=book_1.id, object_model=book_1)
 
         # book should be in the cache
-        self.assertEqual(book_cache.get_book(book_1.id), book_1)
+        self.assertEqual(book_cache.get_object(book_1.id), book_1)
         time.sleep(2)
 
         # book should not be in the cache
-        self.assertEqual(book_cache.get_book(book_1.id), None)
+        self.assertEqual(book_cache.get_object(book_1.id), None)
 
     def test_remove_cache_book(self):
-        book_cache = BookCache(size=3, default_ttl=1)
+        book_cache = Cache(size=3, default_ttl=1)
         book_1 = Book.objects.create(name='name', author='me', pages_amount=5)
-        book_cache.add_book(book_id=book_1.id, book=book_1)
+        book_cache.set_object(object_id=book_1.id, object_model=book_1)
 
         # book should be in the cache
-        self.assertEqual(book_cache.get_book(book_1.id), book_1)
-        book_cache.add_book(book_id=book_1.id, book=book_1)
+        self.assertEqual(book_cache.get_object(book_1.id), book_1)
+        book_cache.set_object(object_id=book_1.id, object_model=book_1)
 
         # book should not be in the cache
-        book_cache.remove_book(book_id=book_1.id)
-        self.assertEqual(book_cache.get_book(book_1.id), None)
+        book_cache.remove_object(object_id=book_1.id)
+        self.assertEqual(book_cache.get_object(book_1.id), None)
 
     def test_invalidate_cache(self):
         # creating books
-        book_cache = BookCache(size=3, default_ttl=60)
+        book_cache = Cache(size=3, default_ttl=60)
         book_1 = Book.objects.create(name='name1', author='me1', pages_amount=1)
         book_2 = Book.objects.create(name='name2', author='me2', pages_amount=2)
-        book_cache.add_book(book_id=book_1.id, book=book_1)
-        book_cache.add_book(book_id=book_2.id, book=book_2)
+        book_cache.set_object(object_id=book_1.id, object_model=book_1)
+        book_cache.set_object(object_id=book_2.id, object_model=book_2)
 
         # making sure there book are in the cache
-        self.assertEqual(book_cache.get_book(book_1.id), book_1)
-        self.assertEqual(book_cache.get_book(book_2.id), book_2)
+        self.assertEqual(book_cache.get_object(book_1.id), book_1)
+        self.assertEqual(book_cache.get_object(book_2.id), book_2)
 
         # clearing the cache
         book_cache.invalidate_cache()
-        self.assertEqual(book_cache.get_book(book_1.id), None)
-        self.assertEqual(book_cache.get_book(book_2.id), None)
+        self.assertEqual(book_cache.get_object(book_1.id), None)
+        self.assertEqual(book_cache.get_object(book_2.id), None)
 
     def test_spam_cache_book(self):
         # creating books
-        book_cache = BookCache(size=3, default_ttl=60)
+        book_cache = Cache(size=3, default_ttl=60)
         info_list = [
             ('name1', 'me1'),
             ('name2', 'me2'),
@@ -63,37 +63,37 @@ class CacheTesting(TestCase):  # django is creating new DB
             Book.objects.create(name=name, author=author, pages_amount=1)
             for name, author in info_list
         ]
-        book_cache.add_book(book_id=books[0].id, book=books[0])
-        book_cache.add_book(book_id=books[1].id, book=books[1])
-        book_cache.add_book(book_id=books[2].id, book=books[2])
+        book_cache.set_object(object_id=books[0].id, object_model=books[0])
+        book_cache.set_object(object_id=books[1].id, object_model=books[1])
+        book_cache.set_object(object_id=books[2].id, object_model=books[2])
 
         # latest in cache should be the first book
-        oldest_book_id, oldest_book_time = book_cache.access_time.peekitem()
-        self.assertEqual(oldest_book_id, books[0].id)
+        oldest_object_id, oldest_book_time = book_cache.access_time.peekitem()
+        self.assertEqual(oldest_object_id, books[0].id)
 
         # latest in cache should be the second book
-        book_cache.add_book(book_id=books[3].id, book=books[3])
-        oldest_book_id, oldest_book_time = book_cache.access_time.peekitem()
-        self.assertEqual(oldest_book_id, books[1].id)
+        book_cache.set_object(object_id=books[3].id, object_model=books[3])
+        oldest_object_id, oldest_book_time = book_cache.access_time.peekitem()
+        self.assertEqual(oldest_object_id, books[1].id)
 
         # latest in cache should be the third book
-        book_cache.add_book(book_id=books[4].id, book=books[4])
-        oldest_book_id, oldest_book_time = book_cache.access_time.peekitem()
-        self.assertEqual(oldest_book_id, books[2].id)
+        book_cache.set_object(object_id=books[4].id, object_model=books[4])
+        oldest_object_id, oldest_book_time = book_cache.access_time.peekitem()
+        self.assertEqual(oldest_object_id, books[2].id)
 
     def test_concurrent_access(self):
-        def get_book_from_cache(book_id: int, result_holder: list) -> Union[Book, None]:
-            result = book_cache.get_book(book_id)
+        def get_book_from_cache(object_id: int, result_holder: list) -> Union[Book, None]:
+            result = book_cache.get_object(object_id)
             result_holder[0] = result
             return result
 
-        def delete_book_from_cache(book_id: int) -> None:
-            book_cache.remove_book(book_id)
+        def delete_book_from_cache(object_id: int) -> None:
+            book_cache.remove_object(object_id)
 
         result_holder = ["not None"]
-        book_cache = BookCache(size=3, default_ttl=1)
+        book_cache = Cache(size=3, default_ttl=1)
         book_1 = Book.objects.create(name='name', author='me', pages_amount=5)
-        book_cache.add_book(book_id=book_1.id, book=book_1)
+        book_cache.set_object(object_id=book_1.id, object_model=book_1)
 
         # Create threads for getting and deleting from the cache
         delete_thread = Thread(target=delete_book_from_cache, args=(book_1.id,))
